262c2f356423a227e4b56ab237621ee91c728587
ombangnc <charlomb@yahoo.fr>
2018-05-03 18:59:44 +0200

    .

--- Monopoly/Views/InfoPlatterView.xaml.cs
+++ Monopoly/Views/InfoPlatterView.xaml.cs
@@ -11,10 +11,5 @@ namespace Monopoly.Views
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-
-        }
     }
 }
--- Monopoly/Views/MiddlePlatterView.xaml.cs
+++ Monopoly/Views/MiddlePlatterView.xaml.cs
@@ -11,5 +11,10 @@ namespace Monopoly.Views
         {
             InitializeComponent();
         }
+
+        private void InfoPlatterView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
